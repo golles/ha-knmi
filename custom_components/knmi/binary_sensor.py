18f2d6ed65afa6327cc779dbf4650b5ae4ea924f
golles <developer@golles.nl>
2021-12-14 19:43:56 +0100

    extra_state_attributes from super

--- custom_components/knmi/binary_sensor.py
+++ custom_components/knmi/binary_sensor.py
@@ -74,7 +74,7 @@ class KnmiBinarySensor(KnmiEntity, BinarySensorEntity):
     @property
     def extra_state_attributes(self):
         """Return the device state attributes."""
-        attributes = {}
+        attributes = super().extra_state_attributes
         for attribute in self._attributes:
             attributes[attribute.get("name", None)] = self.coordinator.data[
                 attribute.get("key", None)
