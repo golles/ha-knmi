b0fb2023b93090fd8478f941faa98aee0b1a8982
golles <developer@golles.nl>
2022-02-05 12:08:32 +0100

    Release 1.1.7

--- custom_components/knmi/const.py
+++ custom_components/knmi/const.py
@@ -27,7 +27,7 @@ from homeassistant.components.weather import (
 # Base component constants.
 NAME = "KNMI"
 DOMAIN = "knmi"
-VERSION = "1.1.6"
+VERSION = "1.1.7"
 ATTRIBUTION = "KNMI Weergegevens via https://weerlive.nl/"
 
 # Platforms.
