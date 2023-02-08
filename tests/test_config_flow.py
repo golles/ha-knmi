870eb35bfe5f44ba880deece61d61aa90e389a17
Sander <developer@golles.nl>
2023-02-08 13:00:02 +0000

    Fix black error

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -23,7 +23,10 @@ from .const import MOCK_CONFIG
 @pytest.fixture(autouse=True)
 def bypass_setup_fixture():
     """Prevent setup."""
-    with patch("custom_components.knmi.async_setup", return_value=True,), patch(
+    with patch(
+        "custom_components.knmi.async_setup",
+        return_value=True,
+    ), patch(
         "custom_components.knmi.async_setup_entry",
         return_value=True,
     ):
