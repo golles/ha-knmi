0a12f85b6e58b0e0add0e9d0961932b8c2af26ab
Sander <developer@golles.nl>
2024-04-19 17:26:45 +0000

    Add test case for real sow

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -271,3 +271,15 @@ async def test_warm_snow_fix(hass: HomeAssistant, mocked_data):
 
     assert await config_entry.async_unload(hass)
     await hass.async_block_till_done()
+
+
+@pytest.mark.fixture("cold_snow.json")
+async def test_real_snow(hass: HomeAssistant, mocked_data):
+    """Test if we return snowy if the API returns snowy and a temp lower than 6."""
+    config_entry = await setup_component(hass)
+
+    state = hass.states.get("weather.knmi_home")
+    assert state.state == ATTR_CONDITION_SNOWY
+
+    assert await config_entry.async_unload(hass)
+    await hass.async_block_till_done()
