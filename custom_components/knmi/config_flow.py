e5044b42504a6e9e247424bcec5db097911a342b
Sander <developer@golles.nl>
2025-08-01 12:29:12 +0000

    Add reconfigure option to update location or API key

--- custom_components/knmi/config_flow.py
+++ custom_components/knmi/config_flow.py
@@ -4,9 +4,10 @@ import logging
 from typing import Any
 
 from homeassistant.config_entries import (
+    SOURCE_RECONFIGURE,
     ConfigEntry,
     ConfigFlow,
-    FlowResult,
+    ConfigFlowResult,
     OptionsFlow,
 )
 from homeassistant.const import (
@@ -30,6 +31,14 @@ from .api import (
 from .const import DEFAULT_SCAN_INTERVAL, DOMAIN
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
+CONFIG_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_NAME): str,
+        vol.Required(CONF_LATITUDE): cv.latitude,
+        vol.Required(CONF_LONGITUDE): cv.longitude,
+        vol.Required(CONF_API_KEY): str,
+    }
+)
 
 
 class KnmiFlowHandler(ConfigFlow, domain=DOMAIN):
@@ -39,47 +48,55 @@ class KnmiFlowHandler(ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
+    ) -> ConfigFlowResult:
         """Handle a flow initialized by the user."""
-        _errors = {}
-        if user_input is not None:
+        errors = {}
+        user_input = user_input or {}
+
+        if user_input:
+            name = user_input[CONF_NAME]
+            api_key = user_input[CONF_API_KEY]
+            latitude = user_input[CONF_LATITUDE]
+            longitude = user_input[CONF_LONGITUDE]
+
             try:
                 await self._validate_user_input(
-                    user_input[CONF_API_KEY],
-                    user_input[CONF_LATITUDE],
-                    user_input[CONF_LONGITUDE],
+                    api_key,
+                    latitude,
+                    longitude,
                 )
             except KnmiApiClientCommunicationError as exception:
                 _LOGGER.error(exception)
-                _errors["base"] = "general"
+                errors["base"] = "general"
             except KnmiApiClientApiKeyError as exception:
                 _LOGGER.error(exception)
-                _errors["base"] = "api_key"
+                errors["base"] = "api_key"
             except KnmiApiRateLimitError as exception:
                 _LOGGER.error(exception)
-                _errors["base"] = "daily_limit"
+                errors["base"] = "daily_limit"
             else:
+                if self.source == SOURCE_RECONFIGURE:
+                    return self.async_update_reload_and_abort(
+                        self._get_reconfigure_entry(),
+                        title=name,
+                        data=user_input,
+                    )
                 return self.async_create_entry(
-                    title=user_input[CONF_NAME], data=user_input
+                    title=name,
+                    data=user_input,
                 )
 
+        default_data = {
+            CONF_NAME: self.hass.config.location_name,
+            CONF_LATITUDE: self.hass.config.latitude,
+            CONF_LONGITUDE: self.hass.config.longitude,
+        }
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(
-                        CONF_NAME, default=self.hass.config.location_name
-                    ): str,
-                    vol.Required(
-                        CONF_LATITUDE, default=self.hass.config.latitude
-                    ): cv.latitude,
-                    vol.Required(
-                        CONF_LONGITUDE, default=self.hass.config.longitude
-                    ): cv.longitude,
-                    vol.Required(CONF_API_KEY): str,
-                }
+            data_schema=self.add_suggested_values_to_schema(
+                CONFIG_SCHEMA, default_data
             ),
-            errors=_errors,
+            errors=errors,
         )
 
     async def _validate_user_input(self, api_key: str, latitude: str, longitude: str):
@@ -87,6 +104,17 @@ class KnmiFlowHandler(ConfigFlow, domain=DOMAIN):
         session = async_create_clientsession(self.hass)
         client = KnmiApiClient(api_key, latitude, longitude, session)
         await client.async_get_data()
+
+    async def async_step_reconfigure(
+        self, _: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle reconfiguration."""
+        data = self._get_reconfigure_entry().data.copy()
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=self.add_suggested_values_to_schema(CONFIG_SCHEMA, data),
+        )
 
     @staticmethod
     @callback
@@ -99,7 +127,7 @@ class KnmiOptionsFlowHandler(OptionsFlow):
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
+    ) -> ConfigFlowResult:
         """Manage the options."""
         if user_input is not None:
             return self.async_create_entry(
