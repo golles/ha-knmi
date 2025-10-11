74ade60543a926ba65f8906a986b6cf85c05d3e0
attila123456 <attila123456@gmail.com>
2025-10-11 20:49:54 +0200

    Update test_weather.py
    Test updated too

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -45,8 +45,8 @@ from . import setup_integration, unload_integration
     [
         ("zonnig", ATTR_CONDITION_SUNNY),
         ("bliksem", ATTR_CONDITION_LIGHTNING),
-        ("regen", ATTR_CONDITION_POURING),
-        ("buien", ATTR_CONDITION_RAINY),
+        ("buien", ATTR_CONDITION_POURING),
+        ("regen", ATTR_CONDITION_RAINY),
         ("hagel", ATTR_CONDITION_HAIL),
         ("mist", ATTR_CONDITION_FOG),
         ("sneeuw", ATTR_CONDITION_SNOWY),
