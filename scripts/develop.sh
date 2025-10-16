56afa8da3259b73ee32de964f39f6e8068751dac
Sander <developer@golles.nl>
2025-10-16 15:30:48 +0000

    Move libraries that are needed to run HA into a dependency-group

--- scripts/develop.sh
+++ scripts/develop.sh
@@ -5,13 +5,13 @@ set -e
 cd "$(dirname "$0")/.."
 
 # Set the path to custom_components
-## This let's us have the structure we want <root>/custom_components/integration_blueprint
+## This let's us have the structure we want <root>/custom_components/integration_name
 ## while at the same time have Home Assistant configuration inside <root>/config
 ## without resulting to symlinks.
 export PYTHONPATH="${PYTHONPATH}:${PWD}/custom_components"
 
 # Install dependencies needed to run Home Assistant
-uv pip install hassil home_assistant_intents mutagen pymicro_vad pyspeex_noise PyTurboJPEG
+uv sync --group runhass
 
 # Start Home Assistant
 uv run hass --config "${PWD}/config" --debug
